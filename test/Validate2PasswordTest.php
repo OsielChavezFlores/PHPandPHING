7068c36b184c408d10503a0074a7e51e59d10f77
Osiel <osielchavez22@gmail.com>
2016-10-17 10:20:46 -0500

    Hola que hay2

--- test/Validate2PasswordTest.php
+++ test/Validate2PasswordTest.php
@@ -1,9 +1,9 @@
 <?php
 	
-	class Validate22PasswordTest extends PHPUnit_Framework_TestCase{
+	class Validate2PasswordTest extends PHPUnit_Framework_TestCase{
 		public function testValidLength(){
 			$valPass = new ValidatePassword();
-			$this->assertFalse($valPass->validLength('1234'));
+			$this->assertFalse($valPass->validLength('1234'))
 		}
 	}
 	
--- test/Validate2PasswordTest.php~
+++ test/Validate2PasswordTest.php~
@@ -1,6 +1,6 @@
 <?php
 	
-	class Validate2PasswordTest extends PHPUnit_Framework_TestCase{
+	class Validate22PasswordTest extends PHPUnit_Framework_TestCase{
 		public function testValidLength(){
 			$valPass = new ValidatePassword();
 			$this->assertFalse($valPass->validLength('1234'));
